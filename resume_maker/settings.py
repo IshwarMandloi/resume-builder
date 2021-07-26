ade4c337d64d6c453796dc13671267f19d3ad5c8
IshwarMandloi <ishwarmandloi25@gmail.com>
2021-07-26 17:59:27 +0530

    resume update

--- resume_maker/settings.py
+++ resume_maker/settings.py
@@ -136,10 +136,10 @@ STATICFILES_DIRS = (
 
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 
 
